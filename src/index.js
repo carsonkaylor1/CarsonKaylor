f56f369d3464d331284211d6f57db686e39dce62
carsonkaylor1 <carsonkaylor@gmail.com>
2020-02-02 16:23:42 -0500

    hash router

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import 'react-mdl/extra/material.js';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import {BrowserRouter} from 'react-router-dom';
+import {HashRouter} from 'react-router-dom';
 
 ReactDOM.render(
 <HashRouter><App /></HashRouter>
