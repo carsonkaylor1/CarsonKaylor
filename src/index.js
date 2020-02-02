4d88e6f7e5f8513569518a7018b8632ccec1f56f
carsonkaylor1 <carsonkaylor@gmail.com>
2020-02-02 15:08:51 -0500

    hash router

--- src/index.js
+++ src/index.js
@@ -5,13 +5,13 @@ import 'react-mdl/extra/material.js';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import {BrowserRouter} from 'react-router-dom';
+import {HashRouter} from 'react-router-dom';
 
 ReactDOM.render(
-<BrowserRouter><App /></BrowserRouter>
+<HashRouter><App /></HashRouter>
 , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//serviceWorker.unregister();
