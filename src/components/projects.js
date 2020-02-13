9ea8be29aa6adc7f47796a27814659eb03eab4b5
carsonkaylor1 <carsonkaylor@gmail.com>
2020-02-13 12:49:30 -0500

    update projects

--- src/components/projects.js
+++ src/components/projects.js
@@ -12,12 +12,37 @@ class Projects extends React.Component{
     // Sets the content of each tab
     toggleCategories(){
 
-        //React Tab
+        //Client Projects Tab
         if(this.state.activeTab === 0){
             return(
-            <div className='projects-grid'>    
+            <div className='projects-grid'> 
                 <Card shadow={25} style={{width: '500px', margin: 'auto'}}>
-                    <a href='https://carsonkaylor1.github.io/VibeMonk/#' rel='noopener noreferrer' target='_blank'><CardTitle style={{color: '000', top:'0', height: '270px', background: 'url(https://i.imgur.com/pNRPx9a.jpg) center / cover'}} ></CardTitle></a>
+                    <CardTitle style={{color: '#fff', height: '270px', background: 'url(https://i.imgur.com/fuXNmfW.jpg) center / cover'}}>
+                    </CardTitle>
+                    <CardText>
+                        <h1 style={{fontSize:'24px', fontFamily:'Quicksand', marginBottom:'.5em', marginTop:'0'}}>Wolfe Podiatry</h1>
+                        <div style={{fontFamily:'Quicksand'}}>
+                            A Podiatrist Website
+                        </div>
+                    </CardText>
+                    <CardActions border>
+                    <a href='https://wolfepodiatry.com/' rel='noopener noreferrer' target='_blank'>
+                        <Button colored style={{marginLeft:'25%', width:'50%'}}>Website</Button>
+                        </a>
+                    </CardActions>
+                    <CardMenu style={{color: '#fff'}}>
+                    </CardMenu>
+                </Card>
+            </div>
+            )
+        }
+
+        //Personal Projects Tab
+        else if(this.state.activeTab === 1){
+            return(
+            <div className='projects-grid'>    
+                <Card shadow={25} style={{width: '500px'}}>
+                    <CardTitle style={{color: '000', top:'0', height: '270px', background: 'url(https://i.imgur.com/pNRPx9a.jpg) center / cover'}} ></CardTitle>
                     <CardText>
                     <h1 style={{fontSize:'24px', fontFamily:'Quicksand', marginBottom:'.5em', marginTop:'0'}}>VibeMonk</h1>
                     <div style={{fontFamily:'Quicksand'}}>
@@ -25,15 +50,18 @@ class Projects extends React.Component{
                     </div> 
                     </CardText>
                     <CardActions border>
+                        <a href='https://carsonkaylor1.github.io/VibeMonk/#' rel='noopener noreferrer' target='_blank'>
+                            <Button colored style={{fontFamily:'Quicksand', width:'50%'}}>Website</Button>
+                        </a>
                         <a href='https://github.com/carsonkaylor1/VibeMonk' rel='noopener noreferrer' target='_blank'>
-                            <Button colored style={{fontFamily:'Quicksand', marginLeft:'25%', width:'50%'}}>Github</Button>
+                            <Button colored style={{fontFamily:'Quicksand', width:'50%'}}>Github</Button>
                         </a>
                     </CardActions>
                     <CardMenu style={{color: '#fff'}}>
                     </CardMenu>
                 </Card>
-                <Card shadow={25} style={{width: '500px', margin: 'auto'}}>
-                    <a href='https://work-break.netlify.com/' rel='noopener noreferrer' target='_blank'><CardTitle style={{color: '000', top:'0', height: '270px', background: 'url(https://i.imgur.com/Puv33fe.jpg) center / cover'}} ></CardTitle></a>
+                <Card shadow={25} style={{width: '500px'}}>
+                    <CardTitle style={{color: '000', top:'0', height: '270px', background: 'url(https://i.imgur.com/Puv33fe.jpg) center / cover'}} ></CardTitle>
                     <CardText>
                     <h1 style={{fontSize:'24px', fontFamily:'Quicksand', marginBottom:'.5em', marginTop:'0'}}>Work/Break Timer</h1>
                     <div style={{fontFamily:'Quicksand'}}>
@@ -41,8 +69,11 @@ class Projects extends React.Component{
                     </div> 
                     </CardText>
                     <CardActions border>
+                        <a href='https://work-break.netlify.com/' rel='noopener noreferrer' target='_blank'>
+                            <Button colored style={{fontFamily:'Quicksand', width:'50%'}}>Website</Button>
+                        </a>
                         <a href='https://github.com/carsonkaylor1/Work-Break' rel='noopener noreferrer' target='_blank'>
-                            <Button colored style={{fontFamily:'Quicksand', marginLeft:'25%', width:'50%'}}>Github</Button>
+                            <Button colored style={{fontFamily:'Quicksand', width:'50%'}}>Github</Button>
                         </a>
                     </CardActions>
                     <CardMenu style={{color: '#fff'}}>
@@ -53,7 +84,7 @@ class Projects extends React.Component{
         }
 
         //Games Tab
-        else if(this.state.activeTab === 1){
+        else if(this.state.activeTab === 2){
             return(
             <div className='projects-grid'> 
                 <Card shadow={25} style={{width: '500px', margin: 'auto'}}>
@@ -78,7 +109,7 @@ class Projects extends React.Component{
         }
 
         //Apps tab
-        else if(this.state.activeTab === 2){
+        else if(this.state.activeTab === 3){
             return(
             <div className='projects-grid'> 
                 <Card shadow={25} style={{width: '500px', margin: 'auto'}}>
@@ -104,8 +135,8 @@ class Projects extends React.Component{
                 {/* Establishes the tabs, and sets the state when a tab is clicked on */}
                 
                 <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({activeTab: tabId})} ripple> 
-                    <Tab style={{color:'white', marginLeft: '25%', fontSize:'24px', fontFamily: 'Quicksand'}}>Websites</Tab>
-                    <Tab style={{color:'white', margin:'auto', fontSize:'24px', fontFamily: 'Quicksand'}}>Games</Tab>
+                    <Tab style={{color:'white', marginLeft: '25%', fontSize:'24px', fontFamily: 'Quicksand', border: 'red'}}>Client Projects</Tab>
+                    <Tab style={{color:'white', margin:'auto', fontSize:'24px', fontFamily: 'Quicksand'}}>Personal Projects</Tab>
                     {/* <Tab style={{color:'white', marginRight:'25%', fontSize:'24px', fontFamily: 'Quicksand'}}>Apps</Tab> */}
                 </Tabs>
                 
